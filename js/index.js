7d6c8e2f349fdfd71ac7ad0cda46f02904819a9d
Bertys <bertysitu@gmail.com>
2016-11-25 13:16:45 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -14,21 +14,7 @@ var app = {
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-        
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        //error raro, Uncaught TypeError: Cannot read property 'querySelector' of null
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    initMap();
     }
 };
 
@@ -40,17 +26,30 @@ app.initialize();
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 document.getElementById("b1").addEventListener("click", function(){
-    //alert("Entrando en b2");
-    var cordx=0;
-    var cordy=0;
-    cordx=document.getElementById("CoordX").value;
-    cordy=document.getElementById("CoordY").value;
-    buscaMasProxima(myArrayParadas,cordx,cordy);
-    //NOTOCAR VA BIEN;
+  window.location.href='#one';
+  jQuery( "#one" ).on( "pageload", buscaParadasbyLinia(myArrayParadass55));
 });
 
+document.getElementById("b2").addEventListener("click", function(){
+  window.location.href='#two';
+  jQuery( "#two" ).on( "pageload", buscaParadasbyLinia());
+});
 document.getElementById("b3").addEventListener("click", function(){
+  window.location.href='#three';
+  jQuery( "#three" ).on( "pageload", buscaParadasbyLinia(myArrayParadass00));
+});
+/*document.getElementById("bs55").addEventListener("click", function(){
+    //Uncaught ReferenceError: initMap11 is not defined //
+    onMapReadys55(myMap);
+});
+document.getElementById("bs00").addEventListener("click", function(){
+    //Uncaught ReferenceError: initMap11 is not defined //
+    onMapReadys00(myMap);
+});
+document.getElementById("b333").addEventListener("click", function(){
     //Uncaught ReferenceError: initMap11 is not defined //
     initMap();
 });
@@ -58,3 +57,8 @@ document.getElementById("b5").addEventListener("click", function(){
     //NOTOCAR VA BIEN;
     getLocation();
 });
+
+document.getElementById("mospar").addEventListener("click", function(){
+    //NOTOCAR VA BIEN;
+    cargaParadas();
+});*/
